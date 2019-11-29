bb5c5e783607ab316c17b883a064473c847496b0
= <=>
2019-11-29 11:41:35 +0300

    Creating hooks to manage our dataase
    dbConn..hook.php hook created and dbInsert.hook.php have been created and tested.

---
+++ hooks/dbConn.hook.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Undocumented function
+ *
+ * @param string $host
+ * @param string $user
+ * @param string $password
+ * @param string $database
+ * @return void
+ */
+function dbConn(
+    $host = 'localhost',
+    $user = 'root',
+    $password = '',
+    $database = 'class_db'
+) {
+    $conn = mysqli_connect(
+        $host,
+        $user,
+        $password,
+        $database
+    );
+
+    if ($conn != true) {
+        die("Failed to conect to 
+                database: <br/>" .
+            mysqli_error($conn));
+    }
+    return $conn;
+}
